e19d38f5f64040cec66db703b83aa674ae8be1d6
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-02-02 10:54:32 -0500

    [ADD] Styled

---
+++ src/GifGridApp.jsx
@@ -0,0 +1,13 @@
+export const GifGridApp = () => {
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifGrid</h1>
+            {/* input */}
+
+            {/* listado de gif */}
+
+                {/* gif item */}
+        </>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { GifGridApp } from "./GifGridApp";
+
+import './GifGridApp';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <GifGridApp />
+    </React.StrictMode>
+);
